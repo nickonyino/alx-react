95731992438ab95fa01e2d243f1af46e6a760382
nick onyino <bahatiluiz@gmail.com>
2024-09-23 18:43:39 +0000

    react

---
+++ 0x05-react_state/task_1/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { StyleSheet, css } from "aphrodite";
+import closeIcon from "../assets/close-icon.png";
+import NotificationItem from "./NotificationItem";
+import PropTypes from "prop-types";
+import NotificationItemShape from "./NotificationItemShape";
+
+class Notifications extends Component {
+  constructor(props) {
+    super(props);
+
+    this.markAsRead = this.markAsRead.bind(this);
+  }
+
+  shouldComponentUpdate(nextProps) {
